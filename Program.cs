8a442d53fd3774507f942bb1e8fb15bc482de9f2
Anjo Tadena <tadena.anjo@gmail.com>
2023-05-02 21:14:02 +0800

    [242-easy]: Solve valid anagram using dictionary

--- Program.cs
+++ Program.cs
@@ -3,3 +3,9 @@
 var containsDuplicate = new ContainsDuplicate();
 
 Console.WriteLine($"Contains duplicate: [1,2,3,4,3,5] => {containsDuplicate.Solution(new int[] { 1, 2, 3, 4, 3, 5 })}");
+
+
+var validAnagrams = new ValidAnagram();
+
+Console.WriteLine($"Valid Anagrams: Input: s = \"anagram\", t = \"nagaram\" => {validAnagrams.Solution("anagram", "nagaram")}");
+
---
+++ ValidAnagram.cs
@@ -0,0 +1,57 @@
+﻿namespace edge_of_nowhere
+{
+    public class ValidAnagram
+    {
+        public bool Solution(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            if (s == t)
+            {
+                return true;
+            }
+
+            Dictionary<char, int> firstChars = new Dictionary<char, int>();
+            Dictionary<char, int> secondChars = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                // First characters sets
+                if (!firstChars.ContainsKey(s[i]))
+                {
+                    firstChars[s[i]] = 0;
+                }
+
+                firstChars[s[i]]++;
+
+                // Second characters sets
+                if (!secondChars.ContainsKey(t[i]))
+                {
+                    secondChars[t[i]] = 0;
+                }
+
+                secondChars[t[i]]++;
+            }
+
+            foreach (char c in firstChars.Keys)
+            {
+                int count = 0;
+
+                if (secondChars.ContainsKey(c))
+                {
+                    count = secondChars[c];
+                }
+
+                if (count != firstChars[c])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
